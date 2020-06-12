f7e284cdb5ea5e73386e47106a7f752713731ae8
ambi-arti <60808454+ambi-arti@users.noreply.github.com>
2020-06-12 05:56:38 +0400

    Even more changes!

--- src/main/java/com/artemius/dwshop/controllers/MerchController.java
+++ src/main/java/com/artemius/dwshop/controllers/MerchController.java
@@ -1,26 +1,31 @@
 package com.artemius.dwshop.controllers;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.artemius.dwshop.entities.Merch;
+import com.artemius.dwshop.entities.MerchSize;
 import com.artemius.dwshop.repositories.MerchRepository;
+import com.artemius.dwshop.repositories.MerchSizeRepository;
 
 @Controller
 public class MerchController {
     
     @Autowired
     private MerchRepository m;
-  //  private Iterable<Merch> merchList = m.findAll();
+    private MerchSizeRepository mS;
+    private Iterable<Merch> merchList = m.findAll();
 
     @RequestMapping("/index")
     public String index(Map<String,Object> model) {
         //model.addAttribute("name", name);
-	Iterable<Merch> merchList = m.findAll();
+	//Iterable<Merch> merchList = m.findAll();
 	model.put("merchList",merchList);
         return "index";
     }
@@ -28,9 +33,16 @@ public class MerchController {
     @RequestMapping("/test")
     public String test(Model model) {
         //model.addAttribute("name", name);
-	Iterable<Merch> merchList = m.findAll();
+	//Iterable<Merch> merchList = m.findAll();
 	model.addAttribute("merchList",merchList);
         return "test";
+    }
+    
+    @RequestMapping("/merchInfo")
+    public String merchInfo(@RequestParam(name = "merchID",required=true)Long merchID, Model model) {
+	Optional<Merch> merch = m.findById(merchID);
+	model.addAttribute("merch",merch);
+	return "merchInfo";
     }
     
     public String carousel (Map<String,Object> model) {
--- src/main/resources/static/scripts/main.js
+++ src/main/resources/static/scripts/main.js
@@ -3,6 +3,23 @@ function resetContents()  {
 	//Resets all the contents to their default view
 }
 
+function sendRequest(type,url,params,target) {
+	const request = new XMLHttpRequest();
+	request.open(type, (url+"?"+params), true);
+	request.addEventListener("readystatechange", () => {
+	    if(request.readyState === 4 && request.status === 200) {
+	    	if (target==null)
+	    		console.log(request.responseText);
+	    	else {
+	    		target.innerHTML=request.responseText;
+	    	}
+	    }
+	    
+	});
+	request.send();
+	return request;
+}
+
 function loadContent() {
 	$('.merchCarousel').slick({
 		infinite: false,
@@ -11,4 +28,12 @@ function loadContent() {
 		dots: true,
 		arrows: false
 	});
+}
+
+function showMerchInfo(merch) {
+	let merchID = merch.getAttribute("merchid");
+	let merchInfo = document.getElementById("merchInfo");
+	//AJAX query: send the merch ID back to server, retrieves a view of merch info with all data set
+	const request = sendRequest("POST","/merchInfo",("merchID="+merchID),merchInfo);
+	console.log(request);
 }
